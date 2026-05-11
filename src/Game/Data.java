165baac678baeb7d0c3cde3d27e24f14a1636920
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-05-11 15:55:03 -0400

    Implement event system

--- src/Game/Data.java
+++ src/Game/Data.java
@@ -9,6 +9,42 @@ public class Data
     private static Surface window;
     private Stack<String[]> events = new Stack<>();
     
+    private Area[] levelOneAreas();
+    private Area[] levelTwoAreas();
+    
+    private int currentArea;
+    private int areaLevel;
+    
+    public static void genAreas()
+    {
+        currentArea = 1;
+        areaLevel = 1;
+        
+        levelOneAreas = new Area[8];
+        for (int i = 0; i < 8; i++)
+        {
+            levelOneAreas[i] = new Area(1);
+            levelOneAreas[i].generateArea();
+        }
+        
+        levelTwoAreas = new Area[4];
+        for (int i = 0; i < 4; i++)
+        {
+            levelTwoAreas[i] = new Area(2);
+            levelTwoAreas[i].generateArea();
+        }
+    }
+    public static Area getArea(int level, int areaNum)
+    {
+        if (level == 1)
+        {
+            return levelOneAreas[areaNum];
+        }
+        else if (level == 2)
+        {
+            return levelTwoAreas[areaNum];
+        }
+    }
     public static void setWindow(Surface win)
     {
         window = win;
@@ -17,11 +53,11 @@ public class Data
     {
         return window;
     }
-    public void pushEvent(String[] event)
+    public static void pushEvent(String[] event)
     {
         events.push(event);
     }
-    public String[] getEvent()
+    public static String[] getEvent()
     {
         if (events.empty())
         {
@@ -29,5 +65,9 @@ public class Data
         }
         
         return events.pop();
+    }
+    public boolean hasEvent()
+    {
+        return events.size() != 0;
     }
 }
