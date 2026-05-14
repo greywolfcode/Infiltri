11b03c8f2182656ea84f290a6d97fdc976dd78c2
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-05-14 18:27:11 -0400

    Add basic event loop

--- src/Frames/AreaMenu.java
+++ src/Frames/AreaMenu.java
@@ -1,5 +1,6 @@
 package Frames;
 
+import Game.Area;
 import Game.Data;
 
 public class AreaMenu extends Frame
@@ -10,10 +11,25 @@ public class AreaMenu extends Frame
     }
     public boolean eventHandeler(String event)
     {
+        switch(event)
+        {
+            case "w":
+                Data.getCurrentArea().selectPrev();
+                break;
+            case "s":
+                Data.getCurrentArea().selectNext();
+                break;
+            case "":
+                Data.getCurrentArea().select();
+                break;
+        }
         
+        drawGraph();
+        return true;
     }
     private void drawGraph()
     {
-        
+        Area area = Data.getCurrentArea();
+        area.render();
     }
 }
