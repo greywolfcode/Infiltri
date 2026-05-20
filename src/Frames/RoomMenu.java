245f7d2c6e6e41a3411d57b02079a55d0387b3a7
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-05-20 16:03:52 -0400

    Make enemies render in room

--- src/Frames/RoomMenu.java
+++ src/Frames/RoomMenu.java
@@ -3,6 +3,7 @@ package Frames;
 import java.util.ArrayList;
 
 import Game.Data;
+import Game.Enemy;
 import Game.Room;
 import Game.Unit;
 
@@ -23,8 +24,13 @@ public class RoomMenu extends Frame
     {
         Data.getWindow().blit(room.getBackground(), 0, 0);
         
+        //blit player sprite
         ArrayList<Unit> units = Data.getParty();
         Unit main = units.get(0);
         Data.getWindow().blit(main.getSprite(), 5, 32-main.getSprite().getHeight());
+        
+        //blit enemy sprite
+        Enemy enemy = room.getEnemy();
+        Data.getWindow().blit(enemy.getSprite(), 64-1-enemy.getSprite().getWidth(), 32-enemy.getSprite().getHeight());
     }
 }
