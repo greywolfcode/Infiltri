6c7990dfe5db1ebcc511020bca88c7990b6c176f
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-05-21 21:50:50 -0400

    Add effect handling to unit

--- src/Game/Unit.java
+++ src/Game/Unit.java
@@ -1,5 +1,7 @@
 package Game;
 
+import java.util.HashSet;
+
 import Graphics.Surface;
 
 public abstract class Unit 
@@ -7,13 +9,47 @@ public abstract class Unit
     protected double health;
     protected int level;
     protected Surface sprite;
+    protected boolean locked = false;
+    protected HashSet<Effect> effects = new HashSet<>();
     
     public void damage(Damage damage)
     {
         health -= damage.amount();
     }
+    public void damage(double amount)
+    {
+        health -= amount;
+    }
     public Surface getSprite()
     {
         return sprite;
     }
+    public void addEffect(Effect effect)
+    {
+        effects.add(effect);
+    }
+    public void removeEffect(Effect effect)
+    {
+        effects.remove(effect);
+    }
+    public void applyEffects(int roundNum)
+    {
+        for (Effect effect:effects)
+        {
+            effect.apply(this, roundNum);
+        }
+    }
+    
+    /**
+     * Prevents further actions from being taken
+     */
+    public void lock()
+    {
+        locked = true;
+    }
+    public void unlock()
+    {
+        locked = false;
+    }
+    
 }
